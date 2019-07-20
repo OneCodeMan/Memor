c18fe22b925155d661ce7c897fcf9831ac9aee99
Dave Gumba <dave_gumba@Yahoo.com>
2019-07-20 17:05:38 -0400

    got the UI down, lots of tweaking needed

--- MMemor/ViewController.swift
+++ MMemor/ViewController.swift
@@ -3,13 +3,41 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var durationInputField: UITextField!
+    @IBOutlet weak var intervalInputField: UITextField!
+
+    @IBAction func startButtonPressed(_ sender: Any) {
+        let durationValue = Float(durationInputField.text ?? "") ?? 0.0
+        let intervalValue = Float(intervalInputField.text ?? "") ?? 0.0
+
+        runTimer(for: TimeInterval(intervalValue), duration: TimeInterval(durationValue))
+    }
+    
+    private struct Constants {
+        static let secondsMultiplier: Double = 60.0
+        static let intervalSound: String = "interval_sound"
+        static let timerSound: String = "timer_sound"
+        static let mp3File: String = "mp3"
+    }
+    
+    var intervalAudioPlayer: AVAudioPlayer?
     var audioPlayer: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         do {
-            if let fileURL = Bundle.main.path(forResource: "sound", ofType: "mp3") {
+            if let fileURL = Bundle.main.path(forResource: Constants.intervalSound, ofType: Constants.mp3File) {
+                intervalAudioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: fileURL))
+            } else {
+                print("no file with specified name exists")
+            }
+        } catch let error {
+            print("can't play audio file \(error)")
+        }
+        
+        do {
+            if let fileURL = Bundle.main.path(forResource: Constants.timerSound, ofType: Constants.mp3File) {
                 audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: fileURL))
             } else {
                 print("no file with specified name exists")
@@ -17,18 +45,24 @@ class ViewController: UIViewController {
         } catch let error {
             print("can't play audio file \(error)")
         }
-
-        runTimer(for: 3.0)
     }
     
-    func runTimer(for minutes: TimeInterval, duration: TimeInterval = 0.0) {
-        let secondsMultiplier = 60.0
-        let seconds = minutes * secondsMultiplier
-        let timer = Timer.scheduledTimer(withTimeInterval: seconds, repeats: true) { timer in
-            self.audioPlayer?.play()
-            print("play")
+    func runTimer(for minutes: TimeInterval, duration: TimeInterval) {
+        let seconds = minutes * Constants.secondsMultiplier
+        let maxCount = Int(duration/minutes)
+        var count = 0
+        let intervalTimer = Timer.scheduledTimer(withTimeInterval: seconds, repeats: true) { timer in
+            
+            if count < maxCount {
+                count += 1
+                self.intervalAudioPlayer?.play()
+            } else {
+                print("reached max count")
+                self.audioPlayer?.play()
+                timer.invalidate()
+            }
+
         }
-        timer.fire()
+        intervalTimer.fire()
     }
 }
-
