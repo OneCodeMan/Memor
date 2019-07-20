3fa73e87efdeb91598cf8437a4eed182a41220b7
Dave Gumba <dave_gumba@Yahoo.com>
2019-07-20 15:33:54 -0400

    bare bones

--- MMemor/ViewController.swift
+++ MMemor/ViewController.swift
@@ -1,12 +1,34 @@
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        do {
+            if let fileURL = Bundle.main.path(forResource: "sound", ofType: "mp3") {
+                audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: fileURL))
+            } else {
+                print("no file with specified name exists")
+            }
+        } catch let error {
+            print("can't play audio file \(error)")
+        }
+
+        runTimer(for: 3.0)
     }
-
-
+    
+    func runTimer(for minutes: TimeInterval, duration: TimeInterval = 0.0) {
+        let secondsMultiplier = 60.0
+        let seconds = minutes * secondsMultiplier
+        let timer = Timer.scheduledTimer(withTimeInterval: seconds, repeats: true) { timer in
+            self.audioPlayer?.play()
+            print("play")
+        }
+        timer.fire()
+    }
 }
 
